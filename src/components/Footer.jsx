17f9b52112c56aba14e42bfb3ff50d3edfc60457
GedasBu <86909912+GedasBu@users.noreply.github.com>
2021-11-23 20:40:39 +0200

    new card page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+return (
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p class="m-0 text-center text-white">
+          Copyright &copy; Your Website 2021
+        </p>
+      </div>
+    </footer>
+  );
+
+    }
+  
+};
+
+export default Footer;
